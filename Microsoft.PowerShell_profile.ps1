0c083a2436cdacc03b199e2f0d4578a937590d9a
Yan-Éric Cossette <10354148+YanEricCossette@users.noreply.github.com>
2022-02-24 15:28:40 -0500

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,46 @@
+function prompt_custom {
+	
+	Import-Module posh-git
+
+	$host.ui.RawUI.WindowTitle = "Current Folder: $pwd"
+	
+	$IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+	Write-host ($(if ($IsAdmin) { ' Elevated ' } else { '' })) -BackgroundColor DarkRed -ForegroundColor White -NoNewline
+
+	Write-Host "$env:USERNAME" -NoNewLine -Foregroundcolor green
+	Write-Host "@" -NoNewLine -Foregroundcolor DarkYellow
+	Write-Host "$env:COMPUTERNAME" -NoNewLine -Foregroundcolor green
+	Write-Host ":" -NoNewLine -Foregroundcolor DarkYellow
+	Write-Host "$pwd" -Foregroundcolor blue
+
+	Try {			
+		$currentBranchName = [regex]::matches($(git branch), "\* ([\w-]*)").captures.groups[1].value
+		Write-Host "($($currentBranchName)) " -NoNewLine -Foregroundcolor DarkYellow
+	}
+	Catch {
+		Write-Host "" -NoNewLine
+	}
+
+	Write-Host "$(Get-Date -Format [HH:mm]) " -NoNewline -Foregroundcolor red
+	"⚡⠀"
+}
+
+function prompt {
+	prompt_custom
+}
+
+# Run conda_start when you want to use conda.
+function conda_start {
+  $CONDA_PATH = "C:\...\conda.exe"
+	#region conda initialize
+	# !! Contents within this block are managed by 'conda init' !!
+	(& $CONDA_PATH "shell.powershell" "hook") | Out-String | Invoke-Expression
+	#endregion
+
+	function prompt {
+		$env = (conda info --json | ConvertFrom-Json).active_prefix_name
+		Write-Host "($env) " -NoNewline -Foregroundcolor green
+		prompt_custom
+	}
+}
